7d08eaaeeabe4cc95f61d255ab17469a0d788f89
Siddharthaaa <timur.horn@gmx.de>
2019-10-14 17:04:10 +0200

    2 new simple models

--- CoTranscr_Simple.py
+++ CoTranscr_Simple.py
@@ -13,7 +13,7 @@ import numpy as np
 
 last_n = 5000
 
-s1 = bs.SimParam("CoTranscription_simple",
+s1 = bs.SimParam("CoTranscription_unbranched",
                  100000, 10001,
                  dict(vsyn = 1, k_el=1,  ki = 0.3, ks = 0.3, kesc = 0.3, d=0.04),
                  dict(Incl = 0, Skip = 0))
@@ -35,11 +35,69 @@ s1.add_reaction("d*Skip", {"Skip":-1})
 s1.add_reaction("d*Incl", {"Incl":-1})
 
 
+s1 = bs.SimParam("CoTranscription_branched_early_inh",
+                 10000, 10001,
+                 dict(vsyn = 1, k_el=1,  ki = 0.3, ks = 0.3, kesc = 0.3, d=0.04),
+                 dict(Incl = 0, Skip = 0))
+
+s1.add_reaction("vsyn", {"p1":1})
+s1.add_reaction("kesc*p1",{"e1":1, "p1":-1})
+for i in range(6):
+    ind = i+1
+    p1 = "p" + str(ind)
+    p2 = "p" + str(ind+1)
+    e1 = "e" + str(ind)
+    e2 = "e" + str(ind+1)
+    s1.add_reaction("k_el*" + p1, {p1:-1, p2:1} )
+    s1.add_reaction("ki *" + p1, {p1:-1, "Incl":1})
+    s1.add_reaction("k_el*" + e1, {e1:-1, e2:1})
+s1.add_reaction("ki *" + p2, {p2:-1, "Incl":1})
+
+for i in range(5,8):
+    p = "p" + str(i)
+    e = "e" + str(i)
+    s1.add_reaction("ks*" + p, {p:-1, "Skip":1})
+    s1.add_reaction("ks*" + e, {e:-1, "Skip":1} )
+
+s1.add_reaction("d*Skip", {"Skip":-1})
+s1.add_reaction("d*Incl", {"Incl":-1})
+
+
+
+s1 = bs.SimParam("CoTranscription_branched_late_inh",
+                 10000, 10001,
+                 dict(vsyn = 1, k_el=1,  ki = 0.3, ks = 0.3, kesc = 0.3, d=0.04),
+                 dict(Incl = 0, Skip = 0))
+
+s1.add_reaction("vsyn", {"p1":1})
+s1.add_reaction("kesc*p3",{"e3":1, "p3":-1})
+for i in range(6):
+    ind = i+1
+    p1 = "p" + str(ind)
+    p2 = "p" + str(ind+1)
+    e1 = "e" + str(ind)
+    e2 = "e" + str(ind+1)
+    s1.add_reaction("k_el*" + p1, {p1:-1, p2:1} )
+    s1.add_reaction("ki *" + p1, {p1:-1, "Incl":1})
+    s1.add_reaction("k_el*" + e1, {e1:-1, e2:1})
+s1.add_reaction("ki *" + p2, {p2:-1, "Incl":1})
+
+for i in range(5,8):
+    p = "p" + str(i)
+    e = "e" + str(i)
+    s1.add_reaction("ks*" + p, {p:-1, "Skip":1})
+    s1.add_reaction("ks*" + e, {e:-1, "Skip":1} )
+
+s1.add_reaction("d*Skip", {"Skip":-1})
+s1.add_reaction("d*Incl", {"Incl":-1})
+
+
+
 psi_means =[]
 psis = []
 incl =[]
 skip = []
-k_els = np.logspace(-1,2,201)
+k_els = np.logspace(-1,2,101)
 for k_el in k_els:
     s1.set_param("k_el", k_el)
     s1.simulate()
