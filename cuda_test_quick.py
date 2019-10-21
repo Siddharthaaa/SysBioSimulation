a469dd4737897bee5232000981f5872c76d5c25b
Siddharthaaa <timur.horn@gmx.de>
2019-10-21 16:42:01 +0200

    testing cuda script
    cuda test

--- cuda_test_quick.py
+++ cuda_test_quick.py
@@ -8,11 +8,36 @@ Created on Wed Aug 21 18:35:42 2019
 import bioch_sim as bs
 import numba as nb
 import numpy as np 
+import time
+
+n = 20
+
 
 sim = bs.get_exmpl_sim()
-sim = bs.get_exmpl_sim("CoTrSplicing")
+sim.set_runtime(1e3)
+sim.set_raster(101)
+sim.simulate_ODE = False
+start = time.time()
+for i in range(n**2):
+#    sim.simulate()
+    pass
+#sim = bs.get_exmpl_sim("CoTrSplicing")
+end = time.time() -start
+print("RUNTIME: ", end)
+
+elong_v = np.ones(n) * 60
+spl_rate = np.ones(n) * 60
+
+v_syn = np.ones(n)* 100
+s1 = np.ones(n) * 1
+
 sim.params
 sim.simulate()
-sim.simulate_cuda({"v_syn": [20, 30, 40, 50], "s1": [0.5, 1, 2, 4]})
+start = time.time()
+res = sim.simulate_cuda({"v_syn": v_syn, "s1": s1})
+end = time.time() -start
+print("RUNTIME: ", end)
+#sim.simulate_cuda({"elong_v": elong_v, "spl_rate": spl_rate})
 
-sim.plot_cuda()
+
+#sim.plot_cuda()
